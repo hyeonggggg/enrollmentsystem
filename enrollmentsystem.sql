42ed436d9e1395bf2d23279a99f7b85947c6781c
hyeonggggg <168175933+hyeonggggg@users.noreply.github.com>
2024-05-14 21:08:27 +0800

    Update enrollmentsystem.sql

--- enrollmentsystem.sql
+++ enrollmentsystem.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 11, 2024 at 03:43 PM
+-- Generation Time: May 14, 2024 at 03:07 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,25 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'admin123');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `enrollment`
 --
 
@@ -39,6 +58,7 @@ CREATE TABLE `enrollment` (
   `firstname` varchar(100) NOT NULL,
   `middlename` varchar(100) NOT NULL,
   `extension` varchar(100) NOT NULL,
+  `sex` varchar(100) NOT NULL,
   `birthday` varchar(100) NOT NULL,
   `age` varchar(100) NOT NULL,
   `placeofbirth` varchar(100) NOT NULL,
@@ -46,6 +66,8 @@ CREATE TABLE `enrollment` (
   `indigenous` varchar(100) NOT NULL,
   `pantawid` varchar(100) NOT NULL,
   `pantawidProgram` varchar(100) NOT NULL,
+  `disability` varchar(100) NOT NULL,
+  `learnerdisability` varchar(100) NOT NULL,
   `region_text` varchar(100) NOT NULL,
   `province_text` varchar(100) NOT NULL,
   `city_text` varchar(100) NOT NULL,
@@ -74,13 +96,18 @@ CREATE TABLE `enrollment` (
 -- Dumping data for table `enrollment`
 --
 
-INSERT INTO `enrollment` (`id`, `schoolyear`, `gradelevel`, `lrn`, `reference`, `balikaral`, `birthnumber`, `lastname`, `firstname`, `middlename`, `extension`, `birthday`, `age`, `placeofbirth`, `ip`, `indigenous`, `pantawid`, `pantawidProgram`, `region_text`, `province_text`, `city_text`, `barangay_text`, `zipcode`, `permanent_region_text`, `permanent_province_text`, `permanent_city_text`, `permanent_barangay_text`, `permanent_zipcode`, `fathers_lastname`, `fathers_firstname`, `fathers_middlename`, `fathers_phonenumber`, `mothers_lastname`, `mothers_firstname`, `mothers_middlename`, `mothers_phonenumber`, `guardians_lastname`, `guardians_firstname`, `guardians_middlename`, `guardians_phonenumber`) VALUES
-(8, '2024 - 2025', 'Grade 11', 'No', '', 'No', '', 'Brosas', 'Kendrix', 'Britiller', '', '2001-05-05', '23', 'Liliw', 'No', '', 'No', '', 'Region IV-A CALABARZON', 'LAGUNA', 'LILIW', 'Kanlurang Bukal', '4004', 'Region IV-A CALABARZON', 'LAGUNA', 'LILIW', 'Kanlurang Bukal', '4004', 'Brosas', 'Alvin', '', '+639664179718', 'Britiller', 'Joselyn', '', '+639213023468', 'Britiller', 'Evelyn', '', '+639664147884'),
-(9, '2024 - 2025', 'Grade 11', 'No', '', 'No', '', 'Brosas', 'Kendrix', 'Britiller', '', '2001-05-05', '23', 'Liliw', 'No', '', 'No', '', 'Region IV-A CALABARZON', 'LAGUNA', 'LILIW', 'Kanlurang Bukal', '4004', 'Region IV-A CALABARZON', 'LAGUNA', 'LILIW', 'Kanlurang Bukal', '4004', 'Brosas', 'Alvin', 'Borlaza', '+639664179718', 'Britiller', 'Joselyn', 'Silva', '+639213023468', 'Britiller', 'Evelyn', 'Silva', '+639664147884');
+INSERT INTO `enrollment` (`id`, `schoolyear`, `gradelevel`, `lrn`, `reference`, `balikaral`, `birthnumber`, `lastname`, `firstname`, `middlename`, `extension`, `sex`, `birthday`, `age`, `placeofbirth`, `ip`, `indigenous`, `pantawid`, `pantawidProgram`, `disability`, `learnerdisability`, `region_text`, `province_text`, `city_text`, `barangay_text`, `zipcode`, `permanent_region_text`, `permanent_province_text`, `permanent_city_text`, `permanent_barangay_text`, `permanent_zipcode`, `fathers_lastname`, `fathers_firstname`, `fathers_middlename`, `fathers_phonenumber`, `mothers_lastname`, `mothers_firstname`, `mothers_middlename`, `mothers_phonenumber`, `guardians_lastname`, `guardians_firstname`, `guardians_middlename`, `guardians_phonenumber`) VALUES
+(11, '2024 - 2025', 'Grade 12', 'Yes', '108297060075', 'No', '', 'Brosas', 'Kendrix', 'Britiller', 'Jr.', 'Male', '2000-06-14', '23', 'Liliw', 'No', '', 'No', '', '', '', 'Region IV-A CALABARZON', 'LAGUNA', 'LILIW', 'KANLURANG BUKAL', '4004', 'Region IV-A CALABARZON', 'LAGUNA', 'LILIW', 'KANLURANG BUKAL', '4002', 'asdasd', 'asdasd', 'asdasda', '+639562511012', 'asdasda', 'adasd', 'adas', '+639562511012', 'asdasd', 'adsasd', 'asdasdas', '+639562511012');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `enrollment`
@@ -93,10 +120,16 @@ ALTER TABLE `enrollment`
 --
 
 --
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `enrollment`
 --
 ALTER TABLE `enrollment`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
